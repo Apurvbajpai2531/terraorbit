6d745e843a1a20e6cde67cee36cb764c08831abf
Apurv Bajpai <apurvbjp@gmail.com>
2026-07-17 12:06:55 +0530

    Day 2: variables, locals, HCL expressions - #TerraWeek

---
+++ locals.tf
@@ -0,0 +1,11 @@
+locals {
+  common_tags = merge({
+    Project     = var.project_name
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+    Challenge   = "TerraWeek"
+  }, var.extra_tags)
+
+  name_prefix = "${var.project_name}-${var.environment}"
+  is_prod     = var.environment == "prod" ? true : false
+}
---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "project_name" {
+  type    = string
+  default = "terraorbit"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "extra_tags" {
+  type    = map(string)
+  default = {}
+}
